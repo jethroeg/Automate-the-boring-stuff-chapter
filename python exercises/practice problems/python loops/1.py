daa576587543f4ac69803febda3f4c4befec832f
jethroeg <troygatbunton@gmail.com>
2022-06-25 20:04:49 +0800

    new

---
+++ python exercises/practice problems/python loops/1.py
@@ -0,0 +1,16 @@
+#print everything in the list
+
+messages = ['It is certain',
+    'It is decidedly so',
+    'Yes definitely',
+    'Reply hazy try again',
+    'Ask again later',
+    'Concentrate and ask again',
+    'My reply is no',
+    'Outlook not so good',
+    'Very doubtful']
+
+for i in messages:
+    print(i)
+
+
